3b00a5ae6eacc44fdd82cec5525d97282be7c963
GustavoFerreir4 <gustavosferreira34@gmail.com>
2025-11-05 11:01:13 -0300

    work desktop

--- challenges/first.java
+++ challenges/first.java
@@ -1,13 +1,14 @@
 import java.time.LocalTime;
+import java.lang.Math;
 public class first {
     // List of primitive types
-    byte byte_data = 127;
-    short short_number = 2; // up to 16 bits
-    int integer_number = 100; // up to 32 bits
-    long long_number = 1024; // up to 64 bits
-    float float_number = 0.23f; // up to 32 bits
-    double double_number = 19.200; //ip to 64 bits
-    boolean bool_data = true; // 1 bit
+    private byte byte_data = 127; // one byte
+    private short short_number = 2; // up to 16 bits
+    private int integer_number = 2147483647; // up to 32 bits
+    private long long_number =  Math.powExact(integer_number, integer_number); // up to 64 bits
+    private float float_number = 0.23f; // up to 32 bits
+    private double double_number = 19.200; //ip to 64 bits
+    private boolean bool_data = true; // 1 bit
 
 
     //methods
@@ -15,6 +16,8 @@ public class first {
     public void main(){
         greetings("This is my  first time writing JAVA code.");
         timeStamp();
+        System.out.println("");
+        System.out.println(stringFyVariables());
     }
 
     public void greetings(String userText){
@@ -25,8 +28,18 @@ public class first {
         LocalTime date = LocalTime.now();
         int hours = date.getHour();
         System.out.println(hours);
-    }https://github.com/GustavoFerreir4/projetos-em-JAVA.git
+    }
 
+    public String stringFyVariables(){
+        String varString = "Byte:" + Byte.toString(byte_data);
+        varString += " Float: " + Float.toString(float_number);
+        varString += " Short: " + Short.toString(short_number);
+        varString += " Long: " + Long.toString(long_number);
+        varString += " Integer: " + Integer.toString(integer_number);
+        varString += " Double: " + Double.toString(double_number);
+        varString += " Boolean: " + Boolean.toString(bool_data) ;
+        return varString;
+    }
 
 }
 
